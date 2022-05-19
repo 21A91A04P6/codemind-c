e5a4889d841482b24ee041d0cf85a86ca73a6eb1
21A91A04P6 <98083918+21A91A04P6@users.noreply.github.com>
2022-05-19 11:32:58 +0530

    #include<stdio.h> int main() {     int n,sum=0,i, avg,count=0;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++)     {     scanf("%d",&a[i]);     sum=sum+a[i];     }     avg=sum/n;     for(i=0;i<n;i++)     {    if(a[i]<=avg)    {        count++;    }     }     printf("%d",count);     return 0; }

---
+++ elements_smaller_than_average.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int n,sum=0,i, avg,count=0;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+    scanf("%d",&a[i]);
+    sum=sum+a[i];
+    }
+    avg=sum/n;
+    for(i=0;i<n;i++)
+    {
+   if(a[i]<=avg)
+   {
+       count++;
+   }
+    }
+    printf("%d",count);
+    return 0;
+}
