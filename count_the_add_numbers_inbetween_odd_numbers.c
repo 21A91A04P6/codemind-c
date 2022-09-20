4ed8f49beef336cd063e45aaf0ff03f7a90c0676
21A91A04P6 <98083918+21A91A04P6@users.noreply.github.com>
2022-09-20 15:56:17 +0530

    #include<stdio.h> int main() {     int i,n,count=0;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=1;i<n-1;i++)     {         if(a[i-1]%2!=0 && a[i+1]%2!=0)         {             if(a[i]%2!=0)             {                 count++;             }         }
        }
        printf("%d",count);
    }

---
+++ count_the_add_numbers_inbetween_odd_numbers.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,count=0;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=1;i<n-1;i++)
+    {
+        if(a[i-1]%2!=0 && a[i+1]%2!=0)
+        {
+            if(a[i]%2!=0)
+            {
+                count++;
+            }
+        }
+       
+    }
+    printf("%d",count);
+}
